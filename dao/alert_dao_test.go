3ab0af3cd05390d582ae7b80bb675329530281bd
iceymoss <ice_moss@163.com>
2024-04-24 14:47:17 +0800

    fix: 调整代码

--- dao/alert_dao_test.go
+++ dao/alert_dao_test.go
@@ -1,15 +1,15 @@
 package dao
 
-import (
-	"fmt"
-	"go-gis-api/internal/models"
-	"testing"
-)
+//import (
+//	"fmt"
+//	"serve/model"
+//	"testing"
+//)
 
-func TestCheckPointInAlertRange(t *testing.T) {
-	isContant := CheckPointInAlertRange(1, models.Point{
-		Longitude: "-74.0060",
-		Latitude:  "41.7128",
-	})
-	fmt.Println(isContant)
-}
+//func TestCheckPointInAlertRange(t *testing.T) {
+//	isContant := CheckPointInAlertRange(1, model.Point{
+//		Longitude: "-74.0060",
+//		Latitude:  "41.7128",
+//	}, "")
+//	fmt.Println(isContant)
+//}
